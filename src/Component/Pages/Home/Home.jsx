786b5a325f2df1d55a1fc02b90d2e060156744bd
MdShaharulIslam <shaharulislam025@gmail.com>
2024-11-30 15:09:11 +0600

    added hero section animation

--- src/Component/Pages/Home/Home.jsx
+++ src/Component/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import bgImg from "../../../../public/bg.gif";
+import heroImg from "../../../../public/hero.png";
 
 const Home = () => {
   return (
@@ -14,24 +15,22 @@ const Home = () => {
             <div className="w-full lg:w-1/2">
               <div className="lg:max-w-lg bg-white bg-opacity-75 p-6 rounded-lg shadow-md">
                 <h1 className="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">
-                  Discover the Perfect Place <br /> for Your{" "}
-                  <span className="text-blue-500">Dream Products</span>
+                Secure & Reliable Payments Made Easy 
+                  <span className="text-blue-500">with CoinsPaid </span>
                 </h1>
                 <p className="mt-3 text-gray-600 dark:text-black">
-                  Explore a wide range of high-quality products tailored to meet your
-                  needs. From essentials to luxuries, find everything in one place at
-                  unbeatable prices.
+                Your trusted platform for seamless global transactions. With SSL-certified security, transparent policies, and integrated payment solutions, CoinsPaid ensures every transaction is swift, safe, and hassle-free.
                 </p>
                 <button className="w-full px-5 py-2 mt-6 text-sm tracking-wider text-white uppercase transition-colors duration-300 transform bg-blue-600 rounded-lg lg:w-auto hover:bg-blue-500">
-                  Shop Now
+                Get Started Now
                 </button>
               </div>
             </div>
             <div className="flex items-center justify-center w-full mt-6 lg:mt-0 lg:w-1/2">
               <img
                 className="w-full h-full lg:max-w-3xl"
-                src="https://merakiui.com/images/components/Catalogue-pana.svg"
-                alt="Catalogue Illustration"
+                src={heroImg}
+                alt="Image"
               />
             </div>
           </div>
